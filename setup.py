bb0fb4a45c9eb7120c248568d1039ffb0087c0a9
AntoScotto <scottoantonela@gmail.com>
2025-11-10 17:31:14 -0300

    Entrega 3 y correccion del repositorio
    En esta entrega se acomodaron y eliminaron carpetas que no eran significativas y se subieron los archivos de la entrega final

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='Parcial Aprendizaje Automatico',
-    packages=find_packages(),
-    description='Proyecto de Aprendizaje Automático enfocado en el desarrollo y evaluación de un modelo de Clasificación Binaria para predecir si un asteroide debe ser catalogado como Potencialmente Peligroso (PHA)',
-    author='Antonela Scotto',
-    version='0.0.1'
-)
